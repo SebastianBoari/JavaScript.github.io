ef950f8da3832ad6fb8ea0b409fe934adf5db19d
sebastianboari <luciosebastianboari@gmail.com>
2022-12-02 16:16:54 -0300

    Clase 1 terminada, empiezo Clase 2

--- js/clase1.js
+++ js/clase1.js
@@ -1,16 +1,58 @@
+/* SACAR COMENTARIO PARA VISUALIZAR EN CONSOLA
+
 // CONSIGNAS
 
 // 1) Muestre un mensaje en consola que diga lo siguiente: "¡MIRÁ MAMÁAA, ESTOY PROGRAMANDO!"
+
 console.log("¡MIRÁ MAMÁAA, ESTOY PROGRAMANDO!");
+
 //2) Hacer una variable que almacene un número par mayor a 10.
+
+let numeroPuntoDos = ( 18 );
+
 // 3) Hacer una variable que almacene un numero impar menor a 100.
+
+let numeroPuntoTres = ( 27 );
+
 // 4) Visualizar en consola la suma de las dos variables numéricas que hicimos.
+
+console.log( numeroPuntoDos + numeroPuntoTres);
+
 // 5) Con un cuadro de diálogo, pregúntele el nombre al usuario que ingresa a la web y guarde su valor en una variable.
-// 6) Visualizar en consola, junto al nombre del usuario, el siguiente mensaje: "Bienvenide a nuestra primera web -usuario-"
+
+let nombreUsuario = prompt("Ingrese su Nombre");
+
+// 6) Visualizar en consola, junto al nombre del usuario, el siguiente mensaje: "Bienvenido a mi primera web -usuario-"
+
+console.log("Bienvenido a nuestra primera web " + nombreUsuario);
+
 // 7) Genere una nueva variable que almacene el número 5 y que se multiplique con el número impar.
+
+ let numeroPuntoSiete = ( 5 * numeroPuntoTres);
+
 // 8) Declare tres variables con nombres aleatorios como valor. Concatene y visualice en consola cada uno de los nombres de las participantes del grupo en un solo console.log
-// 9) Reste 100 a la variable que multiplica por 5 al número primo.
+
+ let nombre1 = ( "Lauta" );
+ let nombre2 = ( "Sebas" );
+ let nombre3 = ( "Agus" );
+
+ console.log( nombre1 + ", " + nombre2 + " y " + nombre3 );
+
+// 9) Reste 100 a la variable que multiplica por 5 al número impar.
+
+ console.log( numeroPuntoSiete - 100 );
+
 // 10) Cotejar el tipo de dato del punto 3 y 5.
 
+// FORMA INCORRECTA
+console.log( typeof numeroPuntoTres typeof nombreUsuario );
+// FORMA INCORRECTA
+console.log( typeof numeroPuntoTres + typeof nombreUsuario );
+// FORMA CORRECTA
+console.log( typeof numeroPuntoTres, typeof nombreUsuario );
+
+// FIN
+
+SACAR COMENTARIO PARA VISUALIZAR EN CONSOLA */ 
 
 
---
+++ js/clase2.js
@@ -0,0 +1,8 @@
+// console.log( true & true  true true && false );
+
+console.log( 10 != "10");
+console.log( 10 != 100);
+
+console.log( 10 !== "10");
+console.log( 10 !== 100);
+console.log( 10 !== 10);
