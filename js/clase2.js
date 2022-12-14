53d377276af799d369d7a252ca4e0650a4e82621
sebastianboari <luciosebastianboari@gmail.com>
2022-12-14 11:40:37 -0300

    Termino de resumir clase2.

--- js/clase2.js
+++ js/clase2.js
@@ -55,8 +55,8 @@ console.log( queso && jamon )
 
 console.log(true && true) // --> true
 console.log( false && false ) // ---> false
-console.log( true && false )
-console.log( false && true)
+console.log( true && false ) // ---> false
+console.log( false && true) // ---> false
 
 // múltiples operandos evaluados con AND LÓGICO
 
@@ -218,28 +218,10 @@ console.log(Boolean("NO"))
 SACAR COMENTARIO */
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// ################################################################################################################################################################################
+// ################################################################################################################################################################################
+// ################################################################################################################################################################################
+// ################################################################################################################################################################################
 
 
 // CONSIGNAS EN CLASE
@@ -306,35 +288,12 @@ if( notaAlumno < 6 && notaAlumno >= 0){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // CONSIGNAS DE TAREA
-
 
 // PARA CADA PUNTO, QUIERO QUE SE EVALÚEN TODOS LOS CASOS POSIBLES PRESENTADOS EN LA EJEMPLIFICACIÓN, ASÍ VERIFICAMOS QUE NUESTROS ALGORITMOS FUNCIONAN EN LA TOTALIDAD DE COMPARACIONES POSIBLES.
 // COMO TODAVÍA NO SABEMOS HACER CÓDIGO REUTILIZABLE, VAMOS A TENER QUE HACER LA CANTIDAD DE VARIABLES Y ESTRUCTURAS NECESARIAS PARA PODER COMPROBAR TODOS LOS CASOS.
 // VAN A TENER QUE CREAR MUCHAS VARIABLES, CON RESPECTO A SUS IDENTIFICADORES, USEN LOS NOMBRES QUE QUIERAN, PERO QUE TENGAN SENTIDO
 // LES RECOMIENDO QUE NO COPIEN Y PEGUEN <3
-
-
-
-
-
-
-
 
 
 // 1) Crear una estructura condicional que compare el valor de una variable con un color del semáforo (rojo, verde o amarillo) y devuelva un mensaje en consola en true si puede avanzar o false si no. Si no se ingresa un color válido, se debe devolver un mensaje en consola que diga: Error: color de semáforo inválido.
@@ -357,12 +316,6 @@ if ( semaforoColor === "Rojo" || semaforoColor === "rojo" || semaforoColor === "
 */
 
 
-
-
-
-
-
-
 // 2) Crear una estructura condicional que compare el valor de una variable con las vocales y devuelva un mensaje en consola "Es una vocal" si letra es una vocal o "No es una vocal" si no lo es.
 // EJEMPLIFICACIÓN DE CASOS
 //     'a' // Es una vocal.
@@ -380,20 +333,11 @@ if ( stringUsuario === "a" || stringUsuario === "e" || stringUsuario === "i" || 
  */
 
 
-
-
-
-
-
-
 // 3) Crear una estructura condicional que compare el valor de una variable con las consonantes y devuelva true si letra es una consonante o false si no lo es
 // EJEMPLIFICACIÓN DE CASOS
 // EJ: 'a' // false
 //     'n' // true
 //     '2130213' // false
-
-
-
 
 
 // 4) Crear una estructura condicional que compare tres valores booleanos: pasoTests, tieneMultasImpagas y pagoImpuestos, y devuelva un mensaje en consola que sea true si una persona está habilitada para renovar su carnet de conducir o false si no. Una persona puede renovar su carnet si pasó los tests, no tiene multas impagas y pagó todos los impuestos.
@@ -419,7 +363,3 @@ if ( stringUsuario === "a" || stringUsuario === "e" || stringUsuario === "i" || 
 // EJEMPLIFICACIÓN DE CASOS
 // esPositivoONegativo(3)  // 'Es positivo'
 // esPositivoONegativo(-5) // 'Es negativo'
-
-
-
-
--- js/clase4.js
+++ js/clase4.js
@@ -129,9 +129,11 @@ suma(10, 799); // La suma se va a realizar pero no se va a visualizar en consola
 // No tienen identificador propio.
 // Su identificador está ligado a la variable que lo contiene.
 
+/*
 const resta = function ( a, b ) {
     return a - b
 }
+*/
 
 // Si quiero llamar y/o ejecutar o invocar a la función, debo utilizar el identificador + paréntesis.
 // En el caso de que tenga parámetros, se pasan los argumentos.
@@ -154,6 +156,7 @@ const resta = function ( a, b ) {
 // Podes hacer retorno implicito.
 // Los parentesis son optativos cuando solo hay un solo parametro.
 
+/*
 // Version normal:
 const division = (a, b) => {
     return a / b;
@@ -167,3 +170,208 @@ console.log( divisionSimplificada( 21, 7 ) );
 // Version simplificada con un solo parametro:
 const multiplicarPorDos = a => a * 2;
 console.log( multiplicarPorDos( 21 ) );
+*/
+
+
+// <---------RETURN--------------->
+
+// Solo se puede utilizar RETURN dentro de una FUNCION. 
+// Si un metodo o elemento se encuentra dentro de una funcion se puede utilizar el RETURN.
+// Por ejemplo: IF y FOR si no se encuentran dentro de una funcion el uso de RETURN esta PROHIBIDO.
+// Todo lo que escribas debajo de un RETURN la funcion lo va a ignorar y nunca se va a ejecutar.
+
+// Ejemplo:
+/*
+function multiplicarPorTres( a ){
+    return a * 3;
+    console.warn( "Multiplicacion por tres" );
+    prompt( "Ingrese otro numero" );
+    alert( "Esto es una alerta debajo de un return" );
+    console.error( "Todo esto esta siendo ignorado" )
+}
+
+console.log( multiplicarPorTres(7) );
+*/
+
+// SALTOS DE LINEA EN UN RETURN
+
+// Como vimos en el punto anterior todo lo que pongamos debajo de un RETURN sera ignorado. Por lo tanto no podemos retornar varios valores de esta forma:
+    // return "Datos del usuario: "
+    // return "Nombre: ${nombre}"
+    // return "Apellido: ${ apellido }""
+
+// Para poder hacer saltos de linea dentro de un return se utiliza "\n"
+
+// Ejemplo:
+/*
+const datosDelUsuario = ( nombre, apellido, edad, direccion ) => {
+    return `Datos del Usuario:\n Nombre: ${ nombre }\n Apellido: ${ apellido }\n Edad: ${ edad }\n Direccion: ${ direccion }`
+}
+
+console.log( datosDelUsuario( "Santiago", "Lukaku", 72, "Chaquenio Palavecino 9192") );
+*/
+
+// ################################################################################################################################################################################
+// ################################################################################################################################################################################
+// ################################################################################################################################################################################
+// ################################################################################################################################################################################
+
+
+// <---------------CONSIGNAS / EJERCICIOS ------------------>
+
+// 1) Crear 4 funciones que reciban 2 parámetros numéricos de cualquier tipo, distinto a 0:
+// Para cada uno de los llamados de las funciones, pasar los dos argumentos numéricos y visualizar el retorno en consola.
+
+// a) la función debe retornar la suma de los dos operandos (Agrego un ejemplo de cada tipo de funcion que vimos hoy):
+
+// Funcion Declarativa:
+function sumaDeclarativa (a, b){
+    return a + b;
+}
+
+console.log( sumaDeclarativa(7, 7) );
+
+// Funcion Anonima:
+const sumaAnonima = function (a, b){
+    return a + b;
+}
+
+console.log( sumaAnonima(7, 7) );
+
+
+// Funcion Arrow:
+const sumaArrow = ( a, b ) => { 
+    return a + b;
+} 
+
+console.log( sumaArrow(7, 7) );
+
+// Funcion Arrow Simplificada:
+const sumaArrowSimplificada = ( a, b ) => a + b;
+
+console.log( sumaArrowSimplificada(7, 7) );
+
+// b) la función debe retornar la resta de los dos opernados (Agrego un ejemplo de cada tipo de funcion que vimos hoy):
+
+// Funcion Declarativa:
+function restaDeclarativa ( a, b ) {
+    return a - b;
+}
+
+console.log( restaDeclarativa(32, 8) );
+
+// Funcion Anonima:
+const restaAnonima = function ( a, b ) {
+    return a - b;
+}
+
+console.log( restaAnonima(32, 8) );
+
+// Funcion Arrow:
+const restaArrow = ( a, b ) => {
+    return a - b;
+}
+
+console.log( restaArrow(32, 8) );
+
+// Funcion Arrow Simplificada:
+const restaArrowSimplificada = ( a, b ) => a - b;
+
+console.log( restaArrowSimplificada( 32, 8) );
+
+// c) la función debe retornar la multiplicación de los dos operandos (Agrego un ejemplo de cada tipo de funcion que vimos hoy):
+
+// Funcion Declarativa:
+function multiplicacionDeclarativa ( a, b ) {
+    return a * b;
+}
+
+console.log( multiplicacionDeclarativa( 6, 6 ) );
+
+// Funcion Anonima:
+const multiplicacionAnonima = function ( a, b ) {
+    return a * b;
+}
+
+console.log( multiplicacionAnonima( 6, 6 ) );
+
+// Funcion Arrow:
+const multiplicacionArrow = ( a, b ) => {
+    return a * b;
+}
+
+console.log( multiplicacionArrow(6, 6 ) );
+
+// Funcion Arrow Simplificada:
+const multiplicacionArrowSimplificada = ( a, b ) => a * b;
+
+console.log( multiplicacionArrowSimplificada(6, 6 ) );
+
+// d) la función debe retornar la división de los dos operandos (Agrego un ejemplo de cada tipo de funcion que vimos hoy):
+
+// Funcion Declarativa:
+function divisionDeclarativa ( a, b ) {
+    return a / b;
+}
+
+console.log( divisionDeclarativa(32, 8) );
+
+// Funcion Anonima:
+const divisionAnonima = function ( a, b ) {
+    return a / b;
+}
+
+console.log( divisionAnonima(32, 8) );
+
+// Funcion Arrow:
+const divisionArrow = ( a, b ) => {
+    return a / b;
+}
+
+console.log( divisionArrow(32, 8) );
+
+// Funcion Arrow Simplificada:
+const divisionArrowSimplificada = ( a, b ) => a / b;
+
+console.log( divisionArrowSimplificada(32, 8) );
+
+// ################################################################################################################################################################################
+// ################################################################################################################################################################################
+
+// 2) Crear una función que reciba los siguientes parámetros: nombre, apellido, edad, direccion y telefono.
+// Retornar como cadena de caracteres, la información del usuario. Podés usar \n para generar saltos de línea.
+// Luego al llamar a la función, pase los 5 argumentos y visualice en consola el dato retornado.
+
+// Ejemplo de la visualización:
+// Los datos del cliente son:
+// Nombre: Carolina,
+// Apellido: Lerner,
+// Edad: 28,
+// Dirección: Calle falsa 123,
+// Teléfono: 11221122.
+
+// Funcion Declarativa:
+function datosDeclarativa ( nombre, apellido, edad, direccion, telefono ) {
+    return `Los datos del cliente son: \n Nombre: ${nombre}\n Apellido: ${apellido}\n Edad: ${edad}\n Direccion: ${direccion}\n Telefono: ${telefono}`;
+}
+
+console.log( datosDeclarativa("Carolina", "Lerner", 28, "Calle Falsa 123", 11221122) );
+
+// Funcion Anonima:
+const datosAnonima = function ( nombre, apellido, edad, direccion, telefono ) {
+    return `Los datos del cliente son: \n Nombre: ${nombre}\n Apellido: ${apellido}\n Edad: ${edad}\n Direccion: ${direccion}\n Telefono: ${telefono}`;
+}
+
+console.log( datosAnonima("Carolina", "Lerner", 28, "Calle Falsa 123", 11221122) );
+
+// Funcion Arrow:
+const datosArrow = ( nombre, apellido, edad, direccion, telefono ) => {
+    return `Los datos del cliente son: \n Nombre: ${nombre}\n Apellido: ${apellido}\n Edad: ${edad}\n Direccion: ${direccion}\n Telefono: ${telefono}`;
+}
+
+console.log( datosArrow("Carolina", "Lerner", 28, "Calle Falsa 123", 11221122) );
+
+// Funcion Arrow Simplificada:
+const datosArrowSimplificada = ( nombre, apellido, edad, direccion, telefono ) => `Los datos del cliente son: \n Nombre: ${nombre}\n Apellido: ${apellido}\n Edad: ${edad}\n Direccion: ${direccion}\n Telefono: ${telefono}`;
+
+console.log( datosArrowSimplificada("Carolina", "Lerner", 28, "Calle Falsa 123", 11221122) );
