ca48f10d37aed5a6627f14aaa71712d88cabd58c
sebastianboari <luciosebastianboari@gmail.com>
2023-01-08 01:27:49 -0300

    Lo hecho hasta el punto 6 en practica2.

--- js/practica2.js
+++ js/practica2.js
@@ -31,6 +31,9 @@
 
 // 6- Crea una función llamada reverseWords que tome como argumento una cadena de texto y devuelva la misma cadena con las palabras en orden inverso. Por ejemplo, reverseWords('Hola mundo') debería devolver 'mundo Hola'.
 
+// Opcion 1: Metodos de cadenas (split, join y reverse)
+// Opcion 2: Bucle for, unsifht y join
+
 // 7- Crea una función llamada sortByLength que tome como argumento un array de cadenas de texto y devuelva el array ordenado de mayor a menor longitud de cadena. Por ejemplo, sortByLength(['Hola', 'Mundo', 'Adiós']) debería devolver ['Adiós', 'Mundo', 'Hola'].
 
 // 8- Crea una función llamada sortByLastLetter que tome como argumento un array de cadenas de texto y devuelva el array ordenado por la última letra de cada cadena. Por ejemplo, sortByLastLetter(['Hola', 'Mundo', 'Adiós']) debería devolver ['Mundo', 'Hola', 'Adiós'].
@@ -136,24 +139,6 @@ function findMinOp3(array) {
 
 findMinOp3(numeros);
 findMinOp3([12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 2]);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // 3- Crea una función llamada findAvg que tome como argumento un array de números y devuelva el promedio de todos los elementos del array. Por ejemplo, findAvg([1, 2, 3]) debería devolver 2.
@@ -285,33 +270,199 @@ console.log(unirTexto(textos));
 
 // 3F- (REDUCE) Dado un array de objetos, utiliza reduce para crear un único objeto con todas las propiedades de los objetos del array.
 
+let geografia = {
+    rusia: 'tundra ártica',
+    canadá: 'tundra ártica',
+    estadosUnidos: 'praderas',
+    china: 'desierto',
+    australia: 'desierto',
+    argentina: 'pampa',
+    brasil: 'selva amazónica',
+    congo: 'selva tropical',
+    indonesia: 'selva tropical',
+    india: 'monzones'
+};
+
+let armas = {
+    revolver: "portátil",
+    escopeta: "caza",
+    ak47: "asalto",
+    sniper: "precisión",
+    bomba: "explosiva",
+    cuchillo: "cuerpo a cuerpo",
+    lanzagranadas: "artillería",
+    bazuca: "anti-tanque",
+    escudo: "defensa",
+    taser: "paralización"
+}
+
+let deTodo = [armas, geografia];
+
+
+function unirObjetos(array){
+    let objetosUnidos = array.reduce((acc, cur) => {
+        return {
+            ...acc,
+            ...cur
+        }
+// Aqui descubri que en vez de 0 va {} para inicializar la acumuladora como un objeto vacio. Con 0 funciona igual porque en cada iteracion se retorna un objeto pero igual creo que debe de ser buena practica dejarlo ya como un objeto vacio.
+    }, 0);
+
+    return objetosUnidos;
+};
+
+console.log(unirObjetos(deTodo));
 
 
 // 3G- (REDUCE) Dado un array de palabras, utiliza reduce para crear un único objeto donde las propiedades sean las palabras del array y los valores sean la longitud de cada palabra.
 
+// A mi parecer el ejercicio mas complicado y estuve como 1 hora dando vueltas por internet para saber como hacer que me devuelva un objeto sin spoilearme de como se hace y entenderlo xd.
+let arrayDePalabras = ['utiliza', 'reduce',  'para', 'crear', 'un', 'único', 'objeto'];
+
+function objetoUnico(array){
+     // Creo una variable que guarde el resultado y le aplicamos reduce al array
+    let unicoObjeto = array.reduce((acc, cur) => {
+        acc[cur] = cur.length;
+        return acc;
+    }, {})
+
+    return unicoObjeto;
+};
+
+console.log(objetoUnico(arrayDePalabras));
+
+
 // 3H- (REDUCE) Dado un array de objetos, utiliza reduce para contar cuántos objetos tienen una propiedad "genero" igual a "femenino".
 
+// Creo una clase porque si no me voy a aburrir de crear objetos pero si igual sin esto esta bien
+class Profesor {
+    constructor(nombre, apellido, edad, sexo) {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.sexo = sexo;
+    }
+};
 
+let profGeografia = new Profesor('Alberto', 'Fernandez', 62, 'masculino');
+let profHistoria = new Profesor('Victoria', 'Juarez', 42, 'femenino');
+let profMatematica = new Profesor('Ricardo', 'Lopez', 54, 'masculino');
+let profFilosofia = new Profesor('Irina', 'Centurion', 27, 'femenino');
+let profQuimica = new Profesor('Cecilia', 'Benavides', 35, 'femenino');
 
+let profesores = [profFilosofia, profGeografia, profHistoria, profMatematica, profQuimica];
 
+function buscarGeneroFemenino(array){
+    let generoFemenino = array.reduce((acc, cur) => {
+        if (cur.sexo == 'femenino'){
+            acc++
+        }
 
+        return acc;
+    }, 0)
 
+    return generoFemenino;
+};
 
-
-
-
-
-
+console.log(buscarGeneroFemenino(profesores));
 
 
 
 // 4- Crea una función llamada arrayToObject que tome como argumento un array de parejas de clave-valor y devuelva un objeto con dichas parejas como propiedades. Por ejemplo, arrayToObject([['nombre', 'Juan'], ['edad', 30]]) debería devolver {nombre: 'Juan', edad: 30}.
 
+// Opcion 1: reduce
+function arrayToObjet(array){
+    let newObjet = array.reduce((acc, cur) => {
+        // Retornamos un objeto
+        return {
+            // Gracias al spread operator o spread syntax podemos unir todo bajo un nuevo objeto 
+            ...acc,
+            ...cur
+        }
+    }, {})
+
+    // Retornamos el objeto que declaramos al principio
+    return newObjet;
+};
+
+console.log(arrayToObjet(deTodo));
+
 // 5- Crea una función llamada countVowels que tome como argumento una cadena de texto y devuelva el número de vocales en dicha cadena. Por ejemplo, countVowels('Hola mundo') debería devolver 4.
+
+// Esta resolucion es la mas optima pero primero estuve como por 2 horas intentando con el metodo some() porque se me habia metido en la cabeza que se debia resolver con eso no se porqué la verdad.
+// Opcion 1: bucle for e includes [ESTA ES LA CORRECTA]
+const vocales = ['a', 'e', 'i', 'o', 'u'];
+
+function countVowels(string) {
+    let incluyeVocales = 0;
+        
+    for (let i = 0; i < string.length; i++) {
+      if (vocales.includes(string[i])) {
+        incluyeVocales++;
+      }
+    }
+  
+    return incluyeVocales;
+};
+  
+console.log(countVowels(profFilosofia.apellido)); 
+  
+// Pero despues de mucho dar vueltas lo resolvi con el some tambien para desquitarme
+// Opcion 2: forEach y some() [TAMBIEN SE PUEDE HACER MAS FACIL CON UN INCLUDES PERO IGUAL ESTA FORMA ES MUY REBUSCADA]
+function countVowels(string){
+    let incluyeVocales = 0;
+    // Como forEach() es un metodo para iterar arrays primero debemos pasar el string por split para pasarlo a un array  
+    string.split('').forEach((letra) => {
+        if (['a', 'e', 'i', 'o', 'u'].some((vocal) => vocal === letra)){
+            incluyeVocales++
+        }
+    })
+
+    return incluyeVocales;
+};
+
+console.log(countVowels(profHistoria.apellido));
+
 
 // 6- Crea una función llamada reverseWords que tome como argumento una cadena de texto y devuelva la misma cadena con las palabras en orden inverso. Por ejemplo, reverseWords('Hola mundo') debería devolver 'mundo Hola'.
 
+// Opcion 1: Metodos de cadenas (split, join y reverse) [CREO QUE ES LA MAS CORRECTA Y LEGIBLE QUE LA OTRA OPCION]
+function reverseWords(string){
+    // Guardo en una variable el string convertido a array 
+    let palabras = string.split(' ');
+
+    // Una vez hecho un array puedo aplicarle reverse() para que invierta el orden de las palabras dentro del array
+    palabras.reverse();
+
+    // Y devuelvo las palabras en una cadena de texto como habia entrado
+    return palabras.join(' ');
+};
+    
+console.log(reverseWords(textoGeografiaArgentina)); 
+
+// Opcion 2: Bucle for, unsifht y join
+function reverseWordsOp2(string) {
+    // Guardo en una variable el string convertido a array 
+    let palabras = string.split(' ');
+    let palabrasInvertidas = [];
+
+    // hago que el ciclo for empiece por el final (googleado obvio porque no me la sabia esa xd)
+    // Y con el unsift pusheamos cada palabra al nuevo array que habia declarado mas arriba
+    for (let i = words.length - 1; i >= 0; i--) {
+      palabrasInvertidas.unshift(palabras[i]);
+    }
+
+    // Y devuelvo las palabras en una cadena de texto como habia entrado
+    return palabrasInvertidas.join(' ');
+};
+  
+console.log(reverseWordsOp2(textoGeografiaArgentina)); 
+  
 // 7- Crea una función llamada sortByLength que tome como argumento un array de cadenas de texto y devuelva el array ordenado de mayor a menor longitud de cadena. Por ejemplo, sortByLength(['Hola', 'Mundo', 'Adiós']) debería devolver ['Adiós', 'Mundo', 'Hola'].
+
+function sortByLength(array){
+    array.forEach()
+};
 
 // 8- Crea una función llamada sortByLastLetter que tome como argumento un array de cadenas de texto y devuelva el array ordenado por la última letra de cada cadena. Por ejemplo, sortByLastLetter(['Hola', 'Mundo', 'Adiós']) debería devolver ['Mundo', 'Hola', 'Adiós'].
 
@@ -321,7 +472,3 @@ console.log(unirTexto(textos));
 
 // 11- Crea una función llamada findLargest que tome como argumento un array de arrays de números y devuelva el número más grande de todos los arrays. Por ejemplo, findLargest([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) debería devolver 9.
 
-// Nota:
-// Ejercicios propuestos por ChatGPT. 
-// Estos ejercicios no fueron corregidos por un profesor, pero como funcionan los tomo como bien hechos. Desconozco si alguno cuenta con alguna mala practica
-// o si alguno estaba pensado para resolverse con algua funcion de orden superior en especifico o algo por el estilo, los hice a mi parecer y con las opciones que se em ocurrieron con los conocimientos que tengo actualmente.
